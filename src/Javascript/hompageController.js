2799e2ebf86a98a34cd7bd294689ae18fb80dcb3
Mikey Muller <michaelmuller799@gmail.com>
2019-11-23 14:46:17 -0500

    Set up basic autocomplete for homepage

---
+++ src/Javascript/hompageController.js
@@ -0,0 +1,65 @@
+const CITIES = ['Portland, OR', 'Boston, MA', 'Chapel Hill, NC'];
+
+export const setUpAutocompleter = function(){ 
+
+    let $locationInput = $('.location-input');
+     
+    $locationInput.autoComplete({
+        minChars: 1,
+        delay: 200,
+        source: function(term, suggest){
+            term = term.toLowerCase();
+            var choices = CITIES;
+            var matches = [];
+            for (let i=0; i<choices.length; i++)
+                if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
+            suggest(matches);
+        },
+        onSelect: function(e, term, item){
+            console.log(term);
+        }
+    });
+}
+
+const searchButtonHandler = function(){
+    let locationText = $('.location-input').val();
+    if(locationText == ''){
+        console.log("empty");
+    }
+    else{
+        validateCity(locationText) ? console.log("success") : console.log("city not found");
+    }
+}
+
+export const setUpPage=function(){
+    $(".location-search-button").on("click", function(){
+        searchButtonHandler();
+    });
+}
+
+const validateCity = function(cityName){
+    let result = false;
+    CITIES.forEach(city =>{
+        if(city == cityName){
+            result = true;
+            return;
+        }
+        city = city.split(",")[0].toLowerCase();
+        cityName = cityName.split(",")[0].toLowerCase();
+        if(city == cityName){
+            result = true;
+            return;
+        }
+    });
+    return result;
+}
+
+
+
+
+
+
+$().ready(function() {
+    setUpPage();
+    setUpAutocompleter();
+})
