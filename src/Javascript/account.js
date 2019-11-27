1d0e7584a5320f3945ec430397dd6eec46db2af4
Katherine Griffin <kag728@gmail.com>
2019-11-27 14:17:26 -0500

    sign up registers accounts

--- src/Javascript/account.js
+++ src/Javascript/account.js
@@ -1,3 +1,18 @@
-$(document).on('click', "#signIn", function(event){
-
-});
+$(document).on('click', "#signIn", async function(event){
+    event.preventDefault();
+    $('#message').html('');
+    let r = new axios.post('http://localhost:3000/account/create',
+    {
+        name: $('#inputFirstName').val(),
+        pass: $('#inputPassword').val(),
+        data: {
+            email: $('#inputEmail').val(),
+            username: $('#inputUsername').val(),
+        }
+    });
+    r.then(response => {
+        window.location.href = "homepage.html";
+    }).catch(error => {
+        $('#message').html('<div><strong>Account already exists</strong></div>');
+    });
+   });
