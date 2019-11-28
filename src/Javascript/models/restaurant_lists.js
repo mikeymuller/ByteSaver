baaf38842bd4d50a54a30beaea9d2a273b2975cd
James Bury <jamesbury77@gmail.com>
2019-11-27 23:32:47 -0500

    created side panel

---
+++ src/Javascript/models/restaurant_lists.js
@@ -0,0 +1,21 @@
+export default [{
+    "User_id": "jamesb3",
+    "Cities": [{
+        "City_id": "Chapel Hill",
+        "Restaurant_lists": {
+            "Burgers": [
+                "Al's Burger Shack",
+                "Hops",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5"
+            ],
+            "Mexican": [
+                "Bandidos",
+                "Monterrey's"
+            ]
+        }
+    }]
+}];
