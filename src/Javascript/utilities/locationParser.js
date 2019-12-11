c37e6617863c19a6be61c5b2edcbe10829079ae0
Mikey Muller <michaelmuller799@gmail.com>
2019-12-10 19:57:46 -0500

    Added location parser

---
+++ src/Javascript/utilities/locationParser.js
@@ -0,0 +1,12 @@
+export const parseLocation = function(location){
+    let splitLocation = location.split(",");
+    let city = splitLocation[0];
+    let state = splitLocation[1].toUpperCase();
+    let cityParts = city.split(" ");
+    let capitalizedCity = [];
+    cityParts.forEach(element => {
+        capitalizedCity.push(element.charAt(0).toUpperCase() + element.slice(1));
+    });
+    city = capitalizedCity.join(" ");
+    return [city, state];
+}
